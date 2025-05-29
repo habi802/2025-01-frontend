ae69a221938170a00ed83e79887dd64749a06c63
habi802 <shb8002@gmail.com>
2025-05-29 13:19:21 +0900

    [feat] html 태그

--- day01/script.js
+++ day01/script.js
@@ -17,10 +17,29 @@ if ($screen && $toggleButton) {
   const cb = clickBtn;
 
   //   $toggleButton.addEventListener('click', function () {
-  //     alert('누르지 맛');
+  //     alert('누르면 깨물어 버린다');
   //   });
   $toggleButton.addEventListener('click', () => {
     stopWatchOn = !stopWatchOn;
     console.log('stopWatchOn:', stopWatchOn);
+    //$toggleButton.innerText = `<b>하하</b>`;
+    $toggleButton.innerHTML = stopWatchOn ? '■' : '▶';
+    //$toggleButton.style.backgroundColor = stopWatchOn ? 'tomato' : 'steelblue';
+    $toggleButton.classList.toggle('stop-color');
+    if (stopWatchOn) {
+      // 첫번째 인자: 콜백함수
+      // 두번째 인자: Number - ms
+      timeInterval = setInterval(() => {
+        seconds++;
+        const mm = String(Math.floor(seconds / 6000) % 60).padStart(2, '0');
+        const ss = String(Math.floor(seconds / 100) % 60).padStart(2, '0');
+        const cs = String(seconds % 100).padStart(2, '0');
+        $screen.innerText = `${mm}:${ss}:${cs}`;
+      }, 10);
+    } else {
+      clearInterval(timeInterval);
+      seconds = 0;
+      $screen.innerHTML = '00:00:00';
+    }
   });
 }
